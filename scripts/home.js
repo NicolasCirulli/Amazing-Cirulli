075ec4e0300fd4c04e24ea003926d330e5c7005d
NicolasCirulli <nicolascirulli2@gmail.com>
2022-12-18 00:44:42 -0300

    Task 3 completa

---
+++ scripts/home.js
@@ -0,0 +1,29 @@
+import { render, crearCards, crearCheckBoxes, filtrar } from './module/funciones.js'
+const main = document.getElementById('main-js')
+const search = document.getElementById('search-js')
+const checkboxes = document.getElementById('checkboxes-js')
+let eventos;
+
+fetch('https://amazing-events.onrender.com/api/events')
+    .then( res => res.json() )
+    .then( ( { events} ) => {
+        eventos = events
+        render( crearCheckBoxes( new Set( events.map( event => event.category) ) ), checkboxes)
+        render( crearCards( events ) , main )
+    } )
+    .catch(err=>console.log(err))
+
+
+search.addEventListener( 'input', () => {
+    let filtrados = filtrar( eventos, search.value )
+    filtrados.length > 0 
+        ? render( crearCards( filtrados ) , main )
+        : main.innerHTML = `<h3 class="text-white text-center"> There are no events matching your search </h3>`
+})
+checkboxes.addEventListener( 'change', () => {
+    let filtrados = filtrar( eventos, search.value )
+    filtrados.length > 0 
+        ? render( crearCards( filtrados ) , main )
+        : main.innerHTML = `<h3 class="text-white text-center"> There are no events matching your search </h3>`
+} )
+
---
+++ scripts/module/funciones.js
@@ -0,0 +1,36 @@
+export function filtrar(eventos, value){
+    let checked = [...document.querySelectorAll( 'input[type="checkbox"]:checked' )].map( check => check.value)
+    return eventos.filter( evento => (checked.includes( evento.category) || checked.length === 0) && evento.name.toLowerCase().includes( value.toLowerCase() ) )
+}
+
+export function render(template, elemento){
+    elemento.innerHTML = template
+}
+
+export function crearCheckBoxes(categorias){
+    let template = ''
+    for( let categoria of categorias){
+        template += `
+        <input type="checkbox" class="btn-check" value="${categoria}" id="check-${categoria.replace(' ','-')}" autocomplete="off">
+        <label class="btn btn-secondary" for="check-${categoria.replace(' ','-')}">${categoria}</label>
+        `
+    }
+    return template
+}
+
+export function crearCards(events){
+    let template = ''
+    for( let {image, name, price, _id:id} of events){
+        template += `
+            <div class="card border-secondary pt-3 col-10 col-md-5 col-xl-3">
+                    <img class="card-img-top w-100 h-50" src="${image}" alt="Title">
+                    <div class="card-body d-flex flex-column">
+                    <h4 class="card-title">${name}</h4>
+                    <p class="card-text">Price : ${price}</p>
+                    <a href="./details.html?id=${id}" class="btn btn-secondary col-4 align-self-center">Details</a>
+                    </div>
+            </div>
+        `
+    }
+    return template
+}
